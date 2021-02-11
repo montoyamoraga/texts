34b1c188d2cacf1c48401d7447f513f43ca2e93c
montoyamoraga <montoyamoraga@gmail.com>
2021-02-11 13:46:52 -0500

    move extra/

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,8 +23,7 @@ FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 
 # static paths
-STATIC_PATHS = ['extra/404.md']
-EXTRA_PATH_METADATA = {'extra/404.md': {'path': '404.md'},}
+STATIC_PATHS = ['extra', 'images']
 
 # Blogroll
 # LINKS = (('Pelican', 'http://getpelican.com/'),
